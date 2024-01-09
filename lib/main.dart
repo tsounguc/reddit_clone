44b417b073245f9991937e82e09e886766e77e39
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-09 09:11:37 -0500

    Add pallete class and use as app theme. Create Screen, Repository, Controller folders. Screen is for the UI part. Repository is going to contain all calls to Firebase or other apis and will have not logic. The controller is going to contain all the logic and going to act as a messenger between the two previous folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:reddit_clone/theme/pallete.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
+        debugShowCheckedModeBanner: false,
+        title: 'Reddit Clone',
+        theme: Pallete.darkModeAppTheme,
         home: const SizedBox());
   }
 }
