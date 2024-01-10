45da2d25202980d1fd4a7a1060db981294a374a1
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-10 13:14:44 -0500

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:reddit_clone/theme/pallete.dart';
-
+import 'package:reddit_clone/firebase_options.dart';
 import 'features/auth/screen/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
