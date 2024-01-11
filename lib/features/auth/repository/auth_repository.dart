75dfe7d50309fcd60c632994a051f581d8630cc6
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-11 09:41:42 -0500

    Save user information into userModel variable when user signs in with google

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -2,7 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:reddit_clone/models/user_model.dart';
 
+import '../../../core/constants/constants.dart';
 import '../../../core/providers/firebase_providers.dart';
 
 // Provider
@@ -39,6 +41,16 @@ class AuthRepository {
       );
       UserCredential userCredential =
           await _auth.signInWithCredential(credential);
+      var user = userCredential.user;
+      UserModel userModel = UserModel(
+        name: user?.displayName ?? 'No Name',
+        profilePic: user?.photoURL ?? Constants.avatarDefault,
+        banner: Constants.bannerDefault,
+        uid: user?.uid ?? '',
+        isAuthenticated: true,
+        karma: 0,
+        awards: [],
+      );
       print(userCredential.user?.email);
     } catch (E) {
       print(E);
