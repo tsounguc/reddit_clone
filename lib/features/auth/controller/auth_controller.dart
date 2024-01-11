22e2cf4442d54c01fd89c69eb70546ff5ee0fb2c
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-11 13:41:48 -0500

    Create userProvider to get updated user data

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -3,19 +3,28 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/features/auth/repository/auth_repository.dart';
 
 import '../../../core/utils.dart';
+import '../../../models/user_model.dart';
+
+final userProvider = StateProvider<UserModel?>((ref) => null);
 
 // Provider
 final authControllerProvider = Provider(((ref) => AuthController(
       authRepository: ref.read(authRepositoryProvider),
+      ref: ref,
     )));
 
 class AuthController {
   final AuthRepository _authRepository;
-  AuthController({required AuthRepository authRepository})
-      : _authRepository = authRepository;
+  final Ref _ref;
+  AuthController({required AuthRepository authRepository, required Ref ref})
+      : _authRepository = authRepository,
+        _ref = ref;
 
   void signInWithGoogle(BuildContext context) async {
     final user = await _authRepository.signInWithGoogle();
-    user.fold((l) => showSnackBar(context, l.message), (r) => null);
+    user.fold(
+        (l) => showSnackBar(context, l.message),
+        (userModel) =>
+            _ref.read(userProvider.notifier).update((state) => userModel));
   }
 }
