a3511b89f41e26ab79fb5f224390b2d91a8f7c71
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-11 09:51:32 -0500

    Create FirebaseConstants

---
+++ lib/core/constants/firebase_constants.dart
@@ -0,0 +1,6 @@
+class FirebaseConstants {
+  static const userCollection = 'users';
+  static const communitiesCollection = 'communities';
+  static const postsCollection = 'post';
+  static const commentsCollection = 'comments';
+}
