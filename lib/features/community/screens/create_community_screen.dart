692f53e903fcd780acec0d0b4e956f45eb0a047b
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-13 08:36:02 -0500

    Display Loader

--- lib/features/community/screens/create_community_screen.dart
+++ lib/features/community/screens/create_community_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/features/community/controller/community_controller.dart';
 
+import '../../../core/common/loader.dart';
 import '../../../theme/pallete.dart';
 
 class CreateCommunityScreen extends ConsumerStatefulWidget {
@@ -20,43 +22,54 @@ class _CreateCommunityScreenState extends ConsumerState<CreateCommunityScreen> {
     super.dispose();
   }
 
+  void createCommunity() {
+    ref.read(communityControllerProvider.notifier).createCommnity(
+          communityNameController.text.trim(),
+          context,
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(communityControllerProvider);
     return Scaffold(
       appBar: AppBar(title: const Text('Create a community')),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            const Align(
-                alignment: Alignment.topLeft, child: Text('Community name')),
-            const SizedBox(height: 10),
-            TextField(
-              controller: communityNameController,
-              decoration: const InputDecoration(
-                hintText: 'r/Community_name',
-                filled: true,
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.all(18),
+      body: isLoading
+          ? const Loader()
+          : Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  const Align(
+                      alignment: Alignment.topLeft,
+                      child: Text('Community name')),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: communityNameController,
+                    decoration: const InputDecoration(
+                      hintText: 'r/Community_name',
+                      filled: true,
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.all(18),
+                    ),
+                    maxLength: 21,
+                  ),
+                  const SizedBox(height: 30),
+                  ElevatedButton(
+                    onPressed: () => createCommunity,
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Pallete.blueColor,
+                        minimumSize: const Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                    child: const Text(
+                      'Create community',
+                      style: TextStyle(color: Pallete.whiteColor, fontSize: 18),
+                    ),
+                  ),
+                ],
               ),
-              maxLength: 21,
             ),
-            const SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Pallete.blueColor,
-                  minimumSize: const Size(double.infinity, 50),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20))),
-              child: const Text(
-                'Create community',
-                style: TextStyle(color: Pallete.whiteColor, fontSize: 18),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
