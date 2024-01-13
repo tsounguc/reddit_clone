c89bc6304ea321566c3e81f4d2c4efc4a577f0c8
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-13 10:03:40 -0500

    Display list of user communites

--- lib/features/home/drawers/community_list_drawer.dart
+++ lib/features/home/drawers/community_list_drawer.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/common/error_text.dart';
 import 'package:routemaster/routemaster.dart';
+
+import '../../../core/common/loader.dart';
+import '../../community/controller/community_controller.dart';
 
 class CommunityListDarwer extends ConsumerWidget {
   const CommunityListDarwer({super.key});
@@ -20,6 +24,30 @@ class CommunityListDarwer extends ConsumerWidget {
               leading: const Icon(Icons.add),
               onTap: () => navigateToCreateCommunity(context),
             ),
+            ref.watch(getUserCommunitiesProvider).when(
+                  data: (userCommunities) {
+                    return Expanded(
+                      child: ListView.builder(
+                          itemCount: userCommunities.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final userCommnunity = userCommunities[index];
+                            return ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage:
+                                    NetworkImage(userCommnunity.avatar),
+                              ),
+                              title: Text('r/${userCommnunity.name}'),
+                              trailing:
+                                  const Icon(Icons.arrow_forward_ios_outlined),
+                              onTap: () {},
+                            );
+                          }),
+                    );
+                  },
+                  error: (error, stackTrack) =>
+                      ErrorText(error: error.toString()),
+                  loading: () => const Loader(),
+                )
           ],
         ),
       ),
