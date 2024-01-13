ea87ce5cc30306bf5e4b82eb82491c2cf4e7b16d
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-13 11:09:19 -0500

    More community screen ui

--- lib/features/community/screens/community_screen.dart
+++ lib/features/community/screens/community_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/common/error_text.dart';
+
+import '../../../core/common/loader.dart';
+import '../controller/community_controller.dart';
 
 class CommunityScreen extends ConsumerWidget {
   final String name;
@@ -7,6 +11,26 @@ class CommunityScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold();
+    return Scaffold(
+      body: ref.watch(getCommunityByNameProvider(name)).when(
+            data: (community) {
+              return NestedScrollView(
+                headerSliverBuilder: (context, innerBoxIsScrolled) {
+                  return [
+                    SliverAppBar(
+                      expandedHeight: 150,
+                      flexibleSpace: Stack(children: [
+                        Positioned.fill(child: Image.network(community.banner))
+                      ]),
+                    )
+                  ];
+                },
+                body: const Text('Displaying posts'),
+              );
+            },
+            error: (error, stackTrace) => ErrorText(error: error.toString()),
+            loading: () => const Loader(),
+          ),
+    );
   }
 }
