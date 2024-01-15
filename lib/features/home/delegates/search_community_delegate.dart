466a72721c110a0cce83ff448462b0489155141a
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-15 14:04:06 -0500

    implemented the methods form SearchCommunityDelegate

--- lib/features/home/delegates/search_community_delegate.dart
+++ lib/features/home/delegates/search_community_delegate.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
+
+import '../../../core/common/error_text.dart';
+import '../../../core/common/loader.dart';
+import '../../../models/community_model.dart';
+import '../../community/controller/community_controller.dart';
 
 class SearchCommunityDelegate extends SearchDelegate {
+  final Ref ref;
+  SearchCommunityDelegate(this.ref);
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
@@ -25,7 +34,26 @@ class SearchCommunityDelegate extends SearchDelegate {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
-    throw UnimplementedError();
+    return ref.watch(searchCommunityProvider(query)).when(
+          data: (communities) {
+            return ListView.builder(
+                itemCount: communities.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final community = communities[index];
+                  return ListTile(
+                    leading: CircleAvatar(
+                        backgroundImage: NetworkImage(community.avatar)),
+                    title: Text('r/${community.name}'),
+                    onTap: () => navigateToCommunity(context, community),
+                  );
+                });
+          },
+          error: (error, stackTrace) => ErrorText(error: error.toString()),
+          loading: () => const Loader(),
+        );
+  }
+
+  void navigateToCommunity(BuildContext context, Community community) {
+    Routemaster.of(context).push('/r/${community.name}');
   }
 }
