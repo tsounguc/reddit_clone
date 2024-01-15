34a90b48d15b59a102260a104e5a714b4faf00d2
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-15 11:05:30 -0500

    Create FirebaseStorage repository and provider

--- lib/core/constants/global_key_provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final globalKeyProvider =
-    Provider((ref) => GlobalKey<ScaffoldMessengerState>());
---
+++ lib/core/providers/storage_repository_provider.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
+
+import '../failure.dart';
+import '../type_defs.dart';
+import 'firebase_providers.dart';
+
+final storageRepositoryProvider = Provider(
+    (ref) => StorageRepository(firebaseStorage: ref.watch(storageProvider)));
+
+class StorageRepository {
+  final FirebaseStorage _firebaseStorage;
+
+  StorageRepository({required FirebaseStorage firebaseStorage})
+      : _firebaseStorage = firebaseStorage;
+
+  FutureEither<String> storeFile(
+      {required String path, required String id, required File? file}) async {
+    try {
+      final reference = _firebaseStorage.ref().child(path).child(id);
+
+      UploadTask uploadTask = reference.putFile(file!);
+
+      final snapshot = await uploadTask;
+
+      return right(await snapshot.ref.getDownloadURL());
+    } catch (e) {
+      return left(Failure(e.toString()));
+    }
+  }
+}
