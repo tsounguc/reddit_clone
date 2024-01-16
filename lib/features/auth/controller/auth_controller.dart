1a1d3c3d6f66e26cf1c49c1587104256bfb53311
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-16 10:03:00 -0500

    Create profile drawer and bind authController's logOut() to logout list tile

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -50,7 +50,7 @@ class AuthController extends StateNotifier<bool> {
     return _authRepository.getUserData(uid);
   }
 
-  void logout() async {
+  void logOut() async {
     _authRepository.logOut();
   }
 }
