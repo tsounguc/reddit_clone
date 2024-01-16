04f56f94601566ee4079abf1af78d67f0306b79d
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-16 17:57:34 -0500

    User can be to mod list

--- lib/features/community/screens/add_mods_screen.dart
+++ lib/features/community/screens/add_mods_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../core/common/error_text.dart';
+import '../../../core/common/loader.dart';
+import '../../../models/community_model.dart';
+import '../../auth/controller/auth_controller.dart';
+import '../controller/community_controller.dart';
+
 class AddModsScreen extends ConsumerStatefulWidget {
   final String name;
   const AddModsScreen({
@@ -13,17 +19,68 @@ class AddModsScreen extends ConsumerStatefulWidget {
 }
 
 class _AddModScreenState extends ConsumerState<AddModsScreen> {
+  Set<String> uids = {};
+  int counter = 0;
+
+  void addUid(String uid) {
+    setState(() {
+      uids.add(uid);
+    });
+  }
+
+  void removeUid(String uid) {
+    setState(() {
+      uids.remove(uid);
+    });
+  }
+
+  void saveMods() {
+    ref
+        .read(communityControllerProvider.notifier)
+        .addModerators(widget.name, uids.toList(), context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: saveMods,
             icon: const Icon(Icons.done),
           )
         ],
       ),
+      body: ref.watch(getCommunityByNameProvider(widget.name)).when(
+            data: (community) => ListView.builder(
+                itemCount: community.members.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final memberUid = community.members[index];
+                  return ref.watch(getUserDataProvider(memberUid)).when(
+                      data: (member) {
+                        if (community.mods.contains(member.uid) &&
+                            counter == 0) {
+                          uids.add(member.uid);
+                        }
+                        counter++;
+                        return CheckboxListTile(
+                            value: uids.contains(member.uid),
+                            onChanged: (value) {
+                              if (value!) {
+                                addUid(member.uid);
+                              } else {
+                                removeUid(member.uid);
+                              }
+                            },
+                            title: Text(member.name));
+                      },
+                      error: (error, stackTrace) =>
+                          ErrorText(error: error.toString()),
+                      loading: () => const Loader());
+                }),
+            error: (error, stackTrace) => ErrorText(error: error.toString()),
+            loading: () => const Loader(),
+          ),
     );
   }
 }
