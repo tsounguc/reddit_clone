dbcda9315d0880bb12118bac339cc0d445926de4
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-16 21:51:55 -0500

    watch themNotifierProvider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class _MyAppState extends ConsumerState<MyApp> {
           data: (data) => MaterialApp.router(
             debugShowCheckedModeBanner: false,
             title: 'Reddit Clone',
-            theme: Pallete.darkModeAppTheme,
+            theme: ref.watch(themeNotifierProvider),
             routerDelegate: RoutemasterDelegate(
               routesBuilder: (context) {
                 if (data != null) {
