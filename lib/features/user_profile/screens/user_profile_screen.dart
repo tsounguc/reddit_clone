d2b271b19d91791eeef32418642fd0fcab28e959
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-16 20:03:57 -0500

    Navigate to EditProfileScreen from UserProfileScreen

--- lib/features/user_profile/screens/user_profile_screen.dart
+++ lib/features/user_profile/screens/user_profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
 
 import '../../../core/common/error_text.dart';
 import '../../../core/common/loader.dart';
@@ -9,6 +10,10 @@ import '../../auth/controller/auth_controller.dart';
 class UserProfileScreen extends ConsumerWidget {
   final String uid;
   const UserProfileScreen({super.key, required this.uid});
+
+  void navigateToEditProfile(BuildContext context) {
+    Routemaster.of(context).push('/edit-profile/$uid');
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -42,7 +47,7 @@ class UserProfileScreen extends ConsumerWidget {
                           alignment: Alignment.bottomLeft,
                           padding: const EdgeInsets.all(20),
                           child: OutlinedButton(
-                            onPressed: () {},
+                            onPressed: () => navigateToEditProfile(context),
                             style: ElevatedButton.styleFrom(
                                 foregroundColor: Pallete.blueColor,
                                 shape: RoundedRectangleBorder(
