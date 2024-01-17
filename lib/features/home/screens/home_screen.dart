95176db39fd29634f71ec84f7fa3d695abaf1e19
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-17 11:21:12 -0500

    display pages on home screen

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,13 +1,29 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/features/home/drawers/profile_drawer.dart';
+import 'package:reddit_clone/theme/pallete.dart';
 
+import '../../../core/constants/constants.dart';
 import '../../auth/controller/auth_controller.dart';
 import '../delegates/search_community_delegate.dart';
 import '../drawers/community_list_drawer.dart';
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  int _currentPage = 0;
+
+  void onPageChange(int page) {
+    setState(() {
+      _currentPage = page;
+    });
+  }
 
   void displayDrawer(BuildContext context) {
     Scaffold.of(context).openDrawer();
@@ -18,8 +34,11 @@ class HomeScreen extends ConsumerWidget {
   }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(
+    BuildContext context,
+  ) {
     final user = ref.watch(userProvider);
+    final currentTheme = ref.watch(themeNotifierProvider);
     return Scaffold(
       appBar: AppBar(
         leading: Builder(
@@ -51,7 +70,16 @@ class HomeScreen extends ConsumerWidget {
       ),
       drawer: const CommunityListDarwer(),
       endDrawer: const ProfileDrawer(),
-      body: Center(child: Text(user!.name)),
+      body: Constants.tabWidgets[_currentPage],
+      bottomNavigationBar: CupertinoTabBar(
+        activeColor: currentTheme.iconTheme.color,
+        backgroundColor: currentTheme.colorScheme.background,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: '')
+        ],
+        onTap: onPageChange,
+      ),
     );
   }
 }
