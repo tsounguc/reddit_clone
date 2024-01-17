75041ae17145ce1c227485e3be40ab5ee203bcee
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-17 11:18:50 -0500

    Put screens in list tabWidgets

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,3 +1,6 @@
+import '../../features/feed/feed_screen.dart';
+import '../../features/post/screens/add_post_screen.dart';
+
 class Constants {
   static const googlePath = 'assets/images/google.png';
   static const loginEmotePath = 'assets/images/loginEmote.png';
@@ -7,4 +10,8 @@ class Constants {
       'https://thumbs.dreamstime.com/b/abstract-stained-pattern-rectangle-background-blue-sky-over-fiery-red-orange-color-modern-painting-art-watercolor-effe-texture-123047399.jpg';
   static const avatarDefault =
       'https://external-preview.redd.it/5kh5OreeLd85QsqYO1Xz_4XSLYwZntfjqou-8fyBFoE.png?auto=webp&s=dbdabd04c399ce9c761ff899f5d38656d1de87c2';
+  static const tabWidgets = [
+    FeedScreen(),
+    AddPostScreen(),
+  ];
 }
