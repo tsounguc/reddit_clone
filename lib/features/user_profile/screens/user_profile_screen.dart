b5389906d7aad3f857452917c9c5912bfe3dacb5
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-16 19:12:07 -0500

    More UserProfileScreen

---
+++ lib/features/user_profile/screens/user_profile_screen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../core/common/error_text.dart';
+import '../../../core/common/loader.dart';
+import '../../../theme/pallete.dart';
+import '../../auth/controller/auth_controller.dart';
+
+class UserProfileScreen extends ConsumerWidget {
+  final String uid;
+  const UserProfileScreen({super.key, required this.uid});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: ref.watch(getUserDataProvider(uid)).when(
+            data: (user) {
+              return NestedScrollView(
+                headerSliverBuilder: (context, innerBoxIsScrolled) {
+                  return [
+                    SliverAppBar(
+                      expandedHeight: 250,
+                      floating: true,
+                      snap: true,
+                      flexibleSpace: Stack(children: [
+                        Positioned.fill(
+                          child: Image.network(
+                            user.banner,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.bottomLeft,
+                          padding:
+                              const EdgeInsets.all(20).copyWith(bottom: 70),
+                          child: CircleAvatar(
+                            backgroundImage: NetworkImage(user.profilePic),
+                            radius: 45,
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.bottomLeft,
+                          padding: const EdgeInsets.all(20),
+                          child: OutlinedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                                foregroundColor: Pallete.blueColor,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 25)),
+                            child: const Text('Edit Profile'),
+                          ),
+                        )
+                      ]),
+                    ),
+                    SliverPadding(
+                      padding: const EdgeInsets.all(16),
+                      sliver: SliverList(
+                        delegate: SliverChildListDelegate(
+                          [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'u/${user.name}',
+                                  style: const TextStyle(
+                                      fontSize: 19,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                            Padding(
+                                padding: const EdgeInsets.only(top: 10),
+                                child: Text('${user.karma} karma')),
+                            const SizedBox(height: 10),
+                            const Divider(thickness: 2),
+                          ],
+                        ),
+                      ),
+                    )
+                  ];
+                },
+                body: const Text('Displaying posts'),
+              );
+            },
+            error: (error, stackTrace) => ErrorText(error: error.toString()),
+            loading: () => const Loader(),
+          ),
+    );
+  }
+}
