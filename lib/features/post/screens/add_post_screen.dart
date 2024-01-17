401bc7d1cb70ddfdac177b578c308d3961cca934
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-17 12:59:44 -0500

    Navigate to AddPostTypeScreen from AddPostScreen

--- lib/features/post/screens/add_post_screen.dart
+++ lib/features/post/screens/add_post_screen.dart
@@ -1,13 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
+
+import '../../../theme/pallete.dart';
 
 class AddPostScreen extends ConsumerWidget {
   const AddPostScreen({super.key});
 
+  void navigateToType(BuildContext context, String type) {
+    Routemaster.of(context).push('/add-post/$type');
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Column(
-      children: [],
+    double cardHeightWidth = 120;
+    double iconSize = 60;
+    final currentTheme = ref.watch(themeNotifierProvider);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        GestureDetector(
+          onTap: () => navigateToType(context, 'image'),
+          child: SizedBox(
+            height: cardHeightWidth,
+            width: cardHeightWidth,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              color: currentTheme.colorScheme.background,
+              elevation: 16,
+              child: Center(
+                child: Icon(Icons.image_outlined, size: iconSize),
+              ),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () => navigateToType(context, 'text'),
+          child: SizedBox(
+            height: cardHeightWidth,
+            width: cardHeightWidth,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              color: currentTheme.colorScheme.background,
+              elevation: 16,
+              child: Center(
+                child: Icon(Icons.font_download_outlined, size: iconSize),
+              ),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () => navigateToType(context, 'link'),
+          child: SizedBox(
+            height: cardHeightWidth,
+            width: cardHeightWidth,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              color: currentTheme.colorScheme.background,
+              elevation: 16,
+              child: Center(
+                child: Icon(Icons.link_outlined, size: iconSize),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
