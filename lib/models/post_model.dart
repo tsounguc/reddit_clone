c02ebe8feed87beaa04e16a346c94f6219a1215e
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-17 15:05:10 -0500

    Create Post model class

---
+++ lib/models/post_model.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+class Post {
+  final String id;
+  final String title;
+  final String? link;
+  final String? description;
+  final String communityName;
+  final String communityProfilePic;
+  final List<String> upvotes;
+  final List<String> downvotes;
+  final int commentCount;
+  final String username;
+  final String uid;
+  final String type;
+  final DateTime createAt;
+  final List<String> awards;
+  Post({
+    required this.id,
+    required this.title,
+    this.link,
+    this.description,
+    required this.communityName,
+    required this.communityProfilePic,
+    required this.upvotes,
+    required this.downvotes,
+    required this.commentCount,
+    required this.username,
+    required this.uid,
+    required this.type,
+    required this.createAt,
+    required this.awards,
+  });
+
+  Post copyWith({
+    String? id,
+    String? title,
+    ValueGetter<String?>? link,
+    ValueGetter<String?>? description,
+    String? communityName,
+    String? communityProfilePic,
+    List<String>? upvotes,
+    List<String>? downvotes,
+    int? commentCount,
+    String? username,
+    String? uid,
+    String? type,
+    DateTime? createAt,
+    List<String>? awards,
+  }) {
+    return Post(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      link: link != null ? link() : this.link,
+      description: description != null ? description() : this.description,
+      communityName: communityName ?? this.communityName,
+      communityProfilePic: communityProfilePic ?? this.communityProfilePic,
+      upvotes: upvotes ?? this.upvotes,
+      downvotes: downvotes ?? this.downvotes,
+      commentCount: commentCount ?? this.commentCount,
+      username: username ?? this.username,
+      uid: uid ?? this.uid,
+      type: type ?? this.type,
+      createAt: createAt ?? this.createAt,
+      awards: awards ?? this.awards,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'link': link,
+      'description': description,
+      'communityName': communityName,
+      'communityProfilePic': communityProfilePic,
+      'upvotes': upvotes,
+      'downvotes': downvotes,
+      'commentCount': commentCount,
+      'username': username,
+      'uid': uid,
+      'type': type,
+      'createAt': createAt.millisecondsSinceEpoch,
+      'awards': awards,
+    };
+  }
+
+  factory Post.fromMap(Map<String, dynamic> map) {
+    return Post(
+      id: map['id'] ?? '',
+      title: map['title'] ?? '',
+      link: map['link'],
+      description: map['description'],
+      communityName: map['communityName'] ?? '',
+      communityProfilePic: map['communityProfilePic'] ?? '',
+      upvotes: List<String>.from(map['upvotes']),
+      downvotes: List<String>.from(map['downvotes']),
+      commentCount: map['commentCount']?.toInt() ?? 0,
+      username: map['username'] ?? '',
+      uid: map['uid'] ?? '',
+      type: map['type'] ?? '',
+      createAt: DateTime.fromMillisecondsSinceEpoch(map['createAt']),
+      awards: List<String>.from(map['awards']),
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Post(id: $id, title: $title, link: $link, description: $description, communityName: $communityName, communityProfilePic: $communityProfilePic, upvotes: $upvotes, downvotes: $downvotes, commentCount: $commentCount, username: $username, uid: $uid, type: $type, createAt: $createAt, awards: $awards)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Post &&
+        other.id == id &&
+        other.title == title &&
+        other.link == link &&
+        other.description == description &&
+        other.communityName == communityName &&
+        other.communityProfilePic == communityProfilePic &&
+        listEquals(other.upvotes, upvotes) &&
+        listEquals(other.downvotes, downvotes) &&
+        other.commentCount == commentCount &&
+        other.username == username &&
+        other.uid == uid &&
+        other.type == type &&
+        other.createAt == createAt &&
+        listEquals(other.awards, awards);
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        title.hashCode ^
+        link.hashCode ^
+        description.hashCode ^
+        communityName.hashCode ^
+        communityProfilePic.hashCode ^
+        upvotes.hashCode ^
+        downvotes.hashCode ^
+        commentCount.hashCode ^
+        username.hashCode ^
+        uid.hashCode ^
+        type.hashCode ^
+        createAt.hashCode ^
+        awards.hashCode;
+  }
+}
