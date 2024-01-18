63bcdec08c87fbf2200b597aaa93ee693364e091
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-17 22:25:18 -0500

    Create PostRepository and define addPost() method

---
+++ lib/features/post/repository/post_repository.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
+
+import '../../../core/constants/firebase_constants.dart';
+import '../../../core/failure.dart';
+import '../../../core/providers/firebase_providers.dart';
+import '../../../core/type_defs.dart';
+import '../../../models/post_model.dart';
+
+final communityRepositoryProvider = Provider((ref) {
+  return PostRepository(firestore: ref.watch(firestoreProvider));
+});
+
+class PostRepository {
+  final FirebaseFirestore _firestore;
+  PostRepository({
+    required FirebaseFirestore firestore,
+  }) : _firestore = firestore;
+
+  FutureVoid addPost(Post post) async {
+    try {
+      return right(_posts.doc(post.id).set(post.toMap()));
+    } on FirebaseException catch (e) {
+      throw e.message!;
+    } catch (e) {
+      return left(Failure(e.toString()));
+    }
+  }
+
+  CollectionReference get _posts =>
+      _firestore.collection(FirebaseConstants.postsCollection);
+}
