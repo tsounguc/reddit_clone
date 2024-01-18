9cc09ec2be0bae80bac5f9db7ecef6100e08caaa
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-18 17:34:20 -0500

    Create PostCard and display in feed_screen

---
+++ lib/core/common/post_card.dart
@@ -0,0 +1,175 @@
+import 'package:any_link_preview/any_link_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/theme/pallete.dart';
+
+import '../../features/auth/controller/auth_controller.dart';
+import '../../models/post_model.dart';
+import '../constants/constants.dart';
+
+class PostCard extends ConsumerWidget {
+  final Post post;
+  const PostCard({super.key, required this.post});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isTypeImage = post.type == 'image';
+    final isTypeLink = post.type == 'link';
+    final isTypeText = post.type == 'text';
+    final user = ref.watch(userProvider)!;
+    final currentTheme = ref.watch(themeNotifierProvider);
+    return Column(children: [
+      Container(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        decoration:
+            BoxDecoration(color: currentTheme.drawerTheme.backgroundColor),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 4,
+                      horizontal: 16,
+                    ).copyWith(right: 0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                CircleAvatar(
+                                  backgroundImage:
+                                      NetworkImage(post.communityProfilePic),
+                                  radius: 16,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 8.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'r/${post.communityName}',
+                                        style: const TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text(
+                                        'u/${post.username}',
+                                        style: const TextStyle(fontSize: 12),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            if (post.uid == user.uid)
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.delete,
+                                  color: Pallete.redColor,
+                                ),
+                              )
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10, bottom: 10),
+                          child: Text(
+                            post.title,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        if (isTypeImage)
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.35,
+                            width: double.infinity,
+                            child: Image.network(
+                              post.link!,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        if (isTypeLink)
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.35,
+                            width: double.infinity,
+                            child: AnyLinkPreview(
+                                displayDirection:
+                                    UIDirection.uiDirectionHorizontal,
+                                link: post.link!),
+                          ),
+                        if (isTypeText)
+                          Container(
+                            alignment: Alignment.bottomLeft,
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: Text(
+                              post.description!,
+                              style: const TextStyle(color: Colors.grey),
+                            ),
+                          ),
+                        Row(
+                          children: [
+                            Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Constants.up,
+                                    size: 30,
+                                    color: post.upvotes.contains(user.uid)
+                                        ? Pallete.redColor
+                                        : null,
+                                  ),
+                                ),
+                                Text(
+                                  '${post.upvotes.length - post.downvotes.length == 0 ? 'Vote' : post.upvotes.length - post.downvotes.length}',
+                                  style: const TextStyle(fontSize: 17),
+                                ),
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Constants.down,
+                                    size: 30,
+                                    color: post.upvotes.contains(user.uid)
+                                        ? Pallete.blueColor
+                                        : null,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.comment,
+                                  ),
+                                ),
+                                Text(
+                                  '${post.commentCount == 0 ? 'Comment' : post.upvotes.length - post.commentCount}',
+                                  style: const TextStyle(fontSize: 17),
+                                ),
+                              ],
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    ]);
+  }
+}
--- lib/features/feed/feed_screen.dart
+++ lib/features/feed/feed_screen.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/features/community/controller/community_controller.dart';
+
+import '../../core/common/error_text.dart';
+import '../../core/common/loader.dart';
+import '../../core/common/post_card.dart';
+import '../auth/controller/auth_controller.dart';
+import '../post/controller/post_controller.dart';
 
 class FeedScreen extends ConsumerWidget {
   const FeedScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    final user = ref.watch(userProvider)!;
+    return ref.watch(getUserCommunitiesProvider(user.uid)).when(
+          data: (communities) => ref.watch(userPostsProvider(communities)).when(
+                data: (posts) {
+                  return ListView.builder(
+                    itemCount: posts.length,
+                    itemBuilder: (context, index) {
+                      final post = posts[index];
+                      return PostCard(post: post);
+                    },
+                  );
+                },
+                error: (error, stackTrace) =>
+                    ErrorText(error: error.toString()),
+                loading: () => const Loader(),
+              ),
+          error: (error, stackTrace) => ErrorText(error: error.toString()),
+          loading: () => const Loader(),
+        );
   }
 }
