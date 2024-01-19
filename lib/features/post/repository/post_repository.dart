1552e091bc41b73d56dada15642b845ece2d9e22
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-19 13:02:20 -0500

    Create upvote() in PostRepository

--- lib/features/post/repository/post_repository.dart
+++ lib/features/post/repository/post_repository.dart
@@ -56,6 +56,42 @@ class PostRepository {
     }
   }
 
+  void upvote(Post post, String userId) async {
+    if (post.downvotes.contains(userId)) {
+      _posts.doc(post.id).update({
+        'downvotes': FieldValue.arrayRemove([userId]),
+      });
+    }
+
+    if (post.upvotes.contains(userId)) {
+      _posts.doc(post.id).update({
+        'upvotes': FieldValue.arrayRemove([userId]),
+      });
+    } else {
+      _posts.doc(post.id).update({
+        'upvotes': FieldValue.arrayUnion([userId]),
+      });
+    }
+  }
+
+  void downvote(Post post, String userId) async {
+    if (post.upvotes.contains(userId)) {
+      _posts.doc(post.id).update({
+        'upvotes': FieldValue.arrayRemove([userId]),
+      });
+    }
+
+    if (post.downvotes.contains(userId)) {
+      _posts.doc(post.id).update({
+        'downvotes': FieldValue.arrayRemove([userId]),
+      });
+    } else {
+      _posts.doc(post.id).update({
+        'downvotes': FieldValue.arrayUnion([userId]),
+      });
+    }
+  }
+
   CollectionReference get _posts =>
       _firestore.collection(FirebaseConstants.postsCollection);
 }
