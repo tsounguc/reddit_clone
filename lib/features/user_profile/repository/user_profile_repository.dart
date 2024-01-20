d25ae12b3fbdc2bdbdda4d6b386f7d09f86642f4
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-20 12:44:01 -0500

    Add getUserPosts() to UserProfileRepository

--- lib/features/user_profile/repository/user_profile_repository.dart
+++ lib/features/user_profile/repository/user_profile_repository.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:reddit_clone/core/failure.dart';
 import 'package:reddit_clone/core/providers/firebase_providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/models/post_model.dart';
 import '../../../core/constants/firebase_constants.dart';
 import '../../../core/type_defs.dart';
 import '../../../models/user_model.dart';
@@ -27,6 +28,20 @@ class UserProfileRepository {
     }
   }
 
+  Stream<List<Post>> getUserPosts(String uid){
+    return _posts.where('uid', isEqualTo: uid).orderBy('createdAt', descending: true).snapshots().map((event){
+      List<Post> userPosts = [];
+      for (var doc in event.docs) {
+        Post post = Post.fromMap(doc.data() as Map<String, dynamic>);
+        userPosts.add(post);
+      }
+      return userPosts;
+    });
+  }
+
   CollectionReference get _users =>
       _firestore.collection(FirebaseConstants.usersCollection);
+  
+   CollectionReference get _posts =>
+      _firestore.collection(FirebaseConstants.postsCollection);
 }
