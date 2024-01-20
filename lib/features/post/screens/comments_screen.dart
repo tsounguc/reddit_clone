202c389537d54f3ec075a4a015766c8ec47253f0
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-20 16:35:48 -0500

    Create comments_screen.dart and watch getPostByIdProvider

---
+++ lib/features/post/screens/comments_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/common/loader.dart';
+import 'package:reddit_clone/core/common/post_card.dart';
+import 'package:reddit_clone/features/post/controller/post_controller.dart';
+
+import '../../../core/common/error_text.dart';
+
+class CommentsScreen extends ConsumerStatefulWidget {
+  final String postId;
+  const CommentsScreen({super.key, required this.postId});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _CommentsScreenState();
+}
+
+class _CommentsScreenState extends ConsumerState<CommentsScreen> {
+  final commentController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    commentController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ref.watch(getPostByIdProvider(widget.postId)).when(
+            data: (post) {
+              return Column(
+                children: [
+                  PostCard(post: post),
+                  TextField(
+                    controller: commentController,
+                    decoration: const InputDecoration(
+                      hintText: 'What are your thoughts?',
+                    ),
+                  )
+                ],
+              );
+            },
+            error: (error, stackTrace) => ErrorText(error: error.toString()),
+            loading: () => const Loader(),
+          ),
+    );
+  }
+}
