7bf21b8a82ed9580b110d27e0fa253d154f08f1c
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-22 11:24:46 -0500

    Display awards when user taps on gift button

--- lib/core/common/post_card.dart
+++ lib/core/common/post_card.dart
@@ -223,7 +223,33 @@ class PostCard extends ConsumerWidget {
                                     loading: () => const Loader(),
                                   ),
                               IconButton(
-                                onPressed: () {},
+                                onPressed: () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) => Dialog(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: GridView.builder(
+                                          shrinkWrap: true,
+                                          gridDelegate:
+                                              const SliverGridDelegateWithFixedCrossAxisCount(
+                                            crossAxisCount: 4,
+                                          ),
+                                          itemCount: user.awards.length,
+                                          itemBuilder: (BuildContext context,
+                                              int index) {
+                                            final award = user.awards[index];
+                                            return Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: Image.asset(
+                                                  Constants.awards[award]!),
+                                            );
+                                          },
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
                                 icon: const Icon(Icons.card_giftcard_outlined),
                               ),
                             ],
