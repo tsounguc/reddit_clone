f0ec187cdc067d3cd3f65a848c2cedf2926cc5a7
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-22 11:33:40 -0500

    Create awardPost() in PostRepository

--- lib/features/post/repository/post_repository.dart
+++ lib/features/post/repository/post_repository.dart
@@ -128,9 +128,29 @@ class PostRepository {
         );
   }
 
+  FutureVoid awardPost(Post post, String award, String senderId) async {
+    try {
+      _posts.doc(post.id).update({
+        'awards': FieldValue.arrayUnion([award])
+      });
+      _users.doc(senderId).update({
+        'awards': FieldValue.arrayRemove([award])
+      });
+      return right(_users.doc(post.uid).update({
+        'awards': FieldValue.arrayUnion([award])
+      }));
+    } on FirebaseException catch (e) {
+      throw e.message!;
+    } catch (e) {
+      return left(Failure(e.toString()));
+    }
+  }
+
   CollectionReference get _posts =>
       _firestore.collection(FirebaseConstants.postsCollection);
 
   CollectionReference get _comments =>
       _firestore.collection(FirebaseConstants.commentsCollection);
+  CollectionReference get _users =>
+      _firestore.collection(FirebaseConstants.usersCollection);
 }
