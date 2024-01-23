9686cd5c0aeb91fcbadf5bed45609e785296b3f9
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-23 10:35:19 -0500

    Restrict community join button in CommunityScreen from guests

--- lib/features/community/screens/community_screen.dart
+++ lib/features/community/screens/community_screen.dart
@@ -34,6 +34,7 @@ class CommunityScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userProvider)!;
+    final isGuest = !user.isAuthenticated;
     return Scaffold(
       body: ref.watch(getCommunityByNameProvider(name)).when(
             data: (community) {
@@ -75,40 +76,45 @@ class CommunityScreen extends ConsumerWidget {
                                       fontSize: 19,
                                       fontWeight: FontWeight.bold),
                                 ),
-                                community.mods.contains(user.uid)
-                                    ? OutlinedButton(
-                                        onPressed: () =>
-                                            navigateToModTools(context),
-                                        style: ElevatedButton.styleFrom(
-                                            foregroundColor: Pallete.blueColor,
-                                            shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(20),
-                                            ),
-                                            padding: const EdgeInsets.symmetric(
-                                                horizontal: 25)),
-                                        child: const Text('Mod Tools'),
-                                      )
-                                    : OutlinedButton(
-                                        onPressed: () =>
-                                            community.members.contains(user.uid)
-                                                ? leaveCommunity(
-                                                    ref, community, context)
-                                                : joinCommunity(
-                                                    ref, community, context),
-                                        style: ElevatedButton.styleFrom(
-                                            foregroundColor: Pallete.blueColor,
-                                            shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(20),
-                                            ),
-                                            padding: const EdgeInsets.symmetric(
-                                                horizontal: 25)),
-                                        child: Text(
-                                            community.members.contains(user.uid)
-                                                ? 'Joined'
-                                                : 'Join'),
-                                      )
+                                if (!isGuest)
+                                  community.mods.contains(user.uid)
+                                      ? OutlinedButton(
+                                          onPressed: () =>
+                                              navigateToModTools(context),
+                                          style: ElevatedButton.styleFrom(
+                                              foregroundColor:
+                                                  Pallete.blueColor,
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(20),
+                                              ),
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 25)),
+                                          child: const Text('Mod Tools'),
+                                        )
+                                      : OutlinedButton(
+                                          onPressed: () => community.members
+                                                  .contains(user.uid)
+                                              ? leaveCommunity(
+                                                  ref, community, context)
+                                              : joinCommunity(
+                                                  ref, community, context),
+                                          style: ElevatedButton.styleFrom(
+                                              foregroundColor:
+                                                  Pallete.blueColor,
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(20),
+                                              ),
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 25)),
+                                          child: Text(community.members
+                                                  .contains(user.uid)
+                                              ? 'Joined'
+                                              : 'Join'),
+                                        )
                               ],
                             ),
                             Padding(
